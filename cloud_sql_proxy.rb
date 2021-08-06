7615a3c1b32a182bd72d2491e4bc8bc5d211fd37
Brian Langenfeld <brian@knotfield.com>
2021-08-06 11:25:40 -0500

    Detect Apple silicon (hopefully). Version bump.

--- cloud_sql_proxy.rb
+++ cloud_sql_proxy.rb
@@ -1,9 +1,9 @@
 class CloudSqlProxy < Formula
   desc "The Cloud SQL Proxy for GoogleCloudPlatform"
   homepage "https://github.com/GoogleCloudPlatform/cloudsql-proxy"
-  version "1.20.2"
-  url "https://github.com/GoogleCloudPlatform/cloudsql-proxy/archive/v#{version}.tar.gz"
-  sha256 "a2dca081982b155ba5bf2604b438321b9d3bc2e9502e944e43ee988359cb7de6"
+  version "1.24.0"
+  url "https://storage.googleapis.com/cloudsql-proxy/v1.24.0/cloud_sql_proxy.darwin.#{`uname -m`}".strip
+  sha256 "e080d3a8d1342176b1afd45c9a898e2b3598e667ff2da6512dbed66da31e1d7a"
   head "https://github.com/GoogleCloudPlatform/cloudsql-proxy.git"
   
   depends_on "go" => :build
